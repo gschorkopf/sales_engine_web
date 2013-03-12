667dea8350435a14377ebb708814284ccb903595
Geoff Schorkopf <gschorkopf@gmail.com>
2013-03-12 13:39:17 -0600

    Added .gitignore and removed files from github

--- lib/sales_engine_web/models/database.rb
+++ lib/sales_engine_web/models/database.rb
@@ -13,16 +13,36 @@ module SalesEngineWeb
       end
     end
 
-    def self.merchants
-      database[:merchants]
-    end
-
     def self.environment=(input)
       @environment = input
     end
 
     def self.environment
       @environment ||= :development
+    end
+
+    def self.merchants
+      database[:merchants]
+    end
+
+    def self.customers
+      database[:customers]
+    end
+    
+    def self.transactions
+      database[:transactions]
+    end
+    
+    def self.items
+      database[:items]
+    end
+    
+    def self.invoices
+      database[:invoices]
+    end
+    
+    def self.invoice_items
+      database[:invoice_items]
     end
   end
 end
